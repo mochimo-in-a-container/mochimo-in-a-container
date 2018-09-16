0a5d76a122e8219c21ab07e5634f07d136bedbcd
Paweł Zmarzły <pawo2500@gmail.com>
2018-09-16 21:45:16 +0200

    Prepare for server address randomization

--- scripts/wallet.sh
+++ scripts/wallet.sh
@@ -7,4 +7,9 @@ if [ ! -e wallet.wal ]; then
     ./wallet -n
 fi
 
+# TODO:
+# mainnet servers are supposed to have IPs from 65.151.42.11 to 65.151.42.16, inclusive
+#RAND=`shuf -i 11-16 -n 1`
+#./wallet -a65.151.42.$RAND -v wallet.wal
+# but as of 16-09-2018, only 65.151.42.11 seem to work
 ./wallet -a65.151.42.11 -v wallet.wal
